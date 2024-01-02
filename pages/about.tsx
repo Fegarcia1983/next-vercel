79285c18c12bc38872ca4dc29ae616d08ac1a99e
Federico Garcia <feg@192.168.1.17>
2024-01-02 12:07:55 -0500

    seccion_02-clase_008

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,18 +4,18 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <>
       <Head>
-        <title>Home | NextApp</title>
-        <meta name="description" content="HomePage" />
+        <title>About | NextApp</title>
+        <meta name="description" content="AboutPage" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <h1 className={`${styles.title}`}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
       </main>
     </>
